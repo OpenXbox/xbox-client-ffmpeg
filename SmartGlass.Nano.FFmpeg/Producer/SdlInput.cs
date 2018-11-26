dce5c6971a61e3541065a2903519d377e0f5ca61
tuxuser <462620+tuxuser@users.noreply.github.com>
2018-11-26 14:37:39 +0100

    Update for upstream compat

--- SmartGlass.Nano.FFmpeg/Producer/SdlInput.cs
+++ SmartGlass.Nano.FFmpeg/Producer/SdlInput.cs
@@ -15,8 +15,8 @@ namespace SmartGlass.Nano.FFmpeg
 
 
         public uint Timestamp { get; private set; }
-        public InputButtons2 Buttons { get; private set; }
-        public InputAnalogue2 Analog { get; private set; }
+        public InputButtons Buttons { get; private set; }
+        public InputAnalogue Analog { get; private set; }
         public InputExtension Extension { get; private set; }
 
         private IntPtr _controller;
@@ -25,8 +25,8 @@ namespace SmartGlass.Nano.FFmpeg
             ControllerMappingFilepath = controllerMappingFilepath;
 
             Timestamp = 0;
-            Buttons = new InputButtons2();
-            Analog = new InputAnalogue2();
+            Buttons = new InputButtons();
+            Analog = new InputAnalogue();
             Extension = new InputExtension();
 
             // Set "controller byte"
@@ -110,13 +110,7 @@ namespace SmartGlass.Nano.FFmpeg
 
         private void HandleControllerButtonChange(NanoGamepadButton button, bool pressed)
         {
-            byte currentValue = Buttons.GetValue(button);
-            bool currentlyPressed = !((currentValue % 2) == 0 || currentValue == 0);
-
-            if (currentlyPressed == pressed)
-                return;
-
-            Buttons.SetValue(button, ++currentValue);
+            Buttons.ToggleButton(button, pressed);
         }
 
         private void HandleControllerAxisChange(NanoGamepadAxis axis, float axisValue)
